a334f11ffe63e35a3680f20bee4959f0f0b55988
hannansiddiqui <hannansiddiqui774@gmail.com>
2024-08-30 02:54:48 +0530

    dashboard page design

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -17,45 +17,47 @@ export default async function Home() {
   const graphData = await getSalesPerMonth();
 
   return (
-    <div className="px-8 py-10">
+    <div className=" px-8 py-10">
       <p className="text-5xl font-bold text-neutral-300">Dashboard</p>
       <Separator className="bg-grey-1 my-5" />
 
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-10">
-        <Card>
+      <div className="mt-10 grid grid-cols-2 md:grid-cols-3 gap-10">
+
+        <Card className="bg-[#121212] text-neutral-400 border border-neutral-300">
           <CardHeader className="flex flex-row justify-between items-center">
-            <CardTitle>Total Revenue</CardTitle>
+            <CardTitle className="text-3xl">Total Revenue</CardTitle>
             <CircleDollarSign className="max-sm:hidden" />
           </CardHeader>
           <CardContent>
-            <p className="text-body-bold">$ {totalRevenue}</p>
+            <p className="text-3xl font-extrabold">$ {totalRevenue}</p>
           </CardContent>
         </Card>
 
-        <Card>
+        <Card className="bg-[#121212] text-neutral-400 border border-neutral-300">
           <CardHeader className="flex flex-row justify-between items-center">
-            <CardTitle>Total Orders</CardTitle>
+            <CardTitle className="text-3xl">Total Orders</CardTitle>
             <ShoppingBag className="max-sm:hidden" />
           </CardHeader>
           <CardContent>
-            <p className="text-body-bold">{totalOrders}</p>
+            <p className="text-3xl font-extrabold">{totalOrders}</p>
           </CardContent>
         </Card>
 
-        <Card>
+
+        <Card className="bg-[#121212] text-neutral-400 border border-neutral-300">
           <CardHeader className="flex flex-row justify-between items-center">
-            <CardTitle>Total Customer</CardTitle>
+            <CardTitle className="text-3xl" >Total Customer</CardTitle>
             <UserRound className="max-sm:hidden" />
           </CardHeader>
           <CardContent>
-            <p className="text-body-bold">{totalCustomers}</p>
+            <p className="text-3xl font-extrabold">{totalCustomers}</p>
           </CardContent>
         </Card>
       </div>
 
-      <Card className="mt-10">
+      <Card className="mt-10 bg-[#121212]">
         <CardHeader>
-          <CardTitle>Sales Chart ($)</CardTitle>
+          <CardTitle className="text-neutral-300 text-3xl font-extrabold" >Sales Chart ($)</CardTitle>
         </CardHeader>
         <CardContent>
           <SalesChart data={graphData} />
